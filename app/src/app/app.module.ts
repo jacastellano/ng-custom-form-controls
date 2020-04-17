369935cd479ecff314cf00f38365708450cec409
José A. Castellano <jacastellanoa@gmail.com>
2020-04-17 17:59:45 +0200

    Add rich text editor base component

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RichTextEditorComponent } from './components/rich-text-editor/rich-text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RichTextEditorComponent
   ],
   imports: [
     BrowserModule
